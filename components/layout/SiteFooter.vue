fe2c15839d691511d6f8594a2b687fccf1dd6ffd
zohreksh <zhr.keshavarz@gmail.com>
2026-09-18 01:35:06 +0330

    feat: redesign foundation footer for NexaStudio layout

--- components/layout/SiteFooter.vue
+++ components/layout/SiteFooter.vue
@@ -1,19 +1,20 @@
 <template>
   <footer class="site-footer">
     <div class="container footer-grid">
-      <div>
-        <h3>Digital Studio</h3>
-        <p>طراحی و ساخت محصولات دیجیتال.</p>
+      <div class="footer-brand">
+        <h3>NexaStudio</h3>
+        <p>ساخت آینده دیجیتال برای کسب‌وکارها</p>
       </div>
       <div>
         <h4>خدمات</h4>
-        <p>طراحی محصول</p>
+        <p>طراحی وب‌سایت و فروشگاه</p>
         <p>توسعه اختصاصی</p>
-        <p>هوش مصنوعی</p>
+        <p>هوش مصنوعی و جستجوی هوشمند</p>
       </div>
       <div>
         <h4>ارتباط</h4>
         <p>تماس با ما</p>
+        <p>شروع همکاری</p>
       </div>
     </div>
   </footer>
