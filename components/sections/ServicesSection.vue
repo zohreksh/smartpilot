74229d0f2e07e7ae0090bec6d2abab045461b72d
zohreksh <zhr.keshavarz@gmail.com>
2026-09-18 18:14:29 +0330

    Adjust services cards icon and text hierarchy

--- components/sections/ServicesSection.vue
+++ components/sections/ServicesSection.vue
@@ -12,6 +12,7 @@
         <div class="service-content">
           <h3>طراحی وب سایت و فروشگاه اینترنتی</h3>
           <p>طراحی مدرن، کاربرپسند و فروش‌محور</p>
+          <a href="#">اطلاعات بیشتر</a>
         </div>
       </article>
       <article class="service-card">
@@ -19,6 +20,7 @@
         <div class="service-content">
           <h3>توسعه سامانه‌های اختصاصی</h3>
           <p>راهکارهای اختصاصی برای نیازهای خاص شما</p>
+          <a href="#">اطلاعات بیشتر</a>
         </div>
       </article>
       <article class="service-card">
@@ -26,6 +28,7 @@
         <div class="service-content">
           <h3>هوش مصنوعی و جستجوی هوشمند</h3>
           <p>تجربه کاربری بهتر و فروش بیشتر</p>
+          <a href="#">اطلاعات بیشتر</a>
         </div>
       </article>
       <article class="service-card">
@@ -33,6 +36,7 @@
         <div class="service-content">
           <h3>پشتیبانی و رشد مستمر</h3>
           <p>همراه شما در تمام مراحل</p>
+          <a href="#">اطلاعات بیشتر</a>
         </div>
       </article>
     </div>
