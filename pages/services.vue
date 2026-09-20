d16bda4b06c20a3ad551081f8ae36d1fb70d295c
zohreksh <zhr.keshavarz@gmail.com>
2026-09-20 17:35:20 +0330

    Restore natural aspect ratio for services proof images

--- pages/services.vue
+++ pages/services.vue
@@ -1434,10 +1434,7 @@ const recoverProofImage = (image) => {
 .proof-products img {
   display: block;
   width: 100%;
-  height: 100%;
-  aspect-ratio: 16 / 10;
-  object-fit: cover;
-  object-position: center;
+  height: auto;
 }
 
 .proof-products figcaption {
