1b0d48c48b6415b3722dd7193e26b5142641b6dd
zohreksh <zhr.keshavarz@gmail.com>
2026-09-20 17:25:12 +0330

    Make services proof images fill cards

--- pages/services.vue
+++ pages/services.vue
@@ -1434,11 +1434,10 @@ const recoverProofImage = (image) => {
 .proof-products img {
   display: block;
   width: 100%;
-  height: auto;
+  height: 100%;
   aspect-ratio: 16 / 10;
-  object-fit: contain;
+  object-fit: cover;
   object-position: center;
-  background: #f8f3ef;
 }
 
 .proof-products figcaption {
